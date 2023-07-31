ea6eadaaad8635cc7fd884114861acf82878966b
__trrbivial_innocence <37957660+trrbivial@users.noreply.github.com>
2023-07-31 22:39:26 +0800

    [WIP] Allow tapenade modules to run ADBench/GMM tests with OpenMP in Fortran language (#7)
    * chore(OpenMP): allow tapenade modules to run ADBench/GMM tests with OpenMP in Fortran language
    
    * chores: fix sources and could run GMM tests without ADBench

--- src/cpp/runner/OutputSave.cpp
+++ src/cpp/runner/OutputSave.cpp
@@ -5,72 +5,75 @@
 
 #include <fstream>
 
-std::string objective_file_name(const std::string& output_prefix, const std::string& input_basename,
-                                const std::string& module_basename)
-{
+std::string objective_file_name(const std::string& output_prefix,
+                                const std::string& input_basename,
+                                const std::string& module_basename) {
     return output_prefix + input_basename + "_F_" + module_basename + ".txt";
 }
 
-std::string jacobian_file_name(const std::string& output_prefix, const std::string& input_basename,
-                               const std::string& module_basename)
-{
+std::string jacobian_file_name(const std::string& output_prefix,
+                               const std::string& input_basename,
+                               const std::string& module_basename) {
     return output_prefix + input_basename + "_J_" + module_basename + ".txt";
 }
 
-void save_time_to_file(const std::string& filepath, const double objective_time, const double derivative_time)
-{
+void save_time_to_file(const std::string& filepath, const double objective_time,
+                       const double derivative_time) {
     std::ofstream out(filepath);
 
-    out << std::scientific << objective_time << std::endl << derivative_time << std::endl;
+    out << std::scientific << objective_time << std::endl
+        << derivative_time << std::endl;
     out.close();
 }
 
-void save_value_to_file(const std::string& filepath, const double& value)
-{
+void save_value_to_file(const std::string& filepath, const double& value) {
     precise_ofstream<std::remove_reference_t<decltype(value)>> out(filepath);
 
     out << value << std::endl;
     out.close();
 }
 
-void save_vector_to_file(const std::string& filepath, const std::vector<double>& gradient)
-{
-    precise_ofstream<std::remove_reference_t<decltype(gradient)>::value_type> out(filepath);
+void save_vector_to_file(const std::string& filepath,
+                         const std::vector<double>& gradient) {
+    precise_ofstream<std::remove_reference_t<decltype(gradient)>::value_type>
+        out(filepath);
 
-    for (const auto& i : gradient)
-    {
+    for (const auto& i : gradient) {
         out << i << std::endl;
     }
 
     out.close();
 }
 
-void save_errors_to_file(const std::string& filepath, const std::vector<double>& reprojection_error,
-                         const std::vector<double>& zach_weight_error)
-{
-    precise_ofstream<std::remove_reference_t<decltype(reprojection_error)>::value_type> out(filepath);
+void save_errors_to_file(const std::string& filepath,
+                         const std::vector<double>& reprojection_error,
+                         const std::vector<double>& zach_weight_error) {
+    precise_ofstream<
+        std::remove_reference_t<decltype(reprojection_error)>::value_type>
+        out(filepath);
 
     out << "Reprojection error:" << std::endl;
-    for (const auto& i : reprojection_error)
-    {
+    for (const auto& i : reprojection_error) {
         out << i << std::endl;
     }
 
     out << "Zach weight error:" << std::endl;
-    for (const auto& i : zach_weight_error)
-    {
+    for (const auto& i : zach_weight_error) {
         out << i << std::endl;
     }
 
     out.close();
 }
 
-void save_jacobian_to_file(const std::string& filepath, int jacobian_ncols, int jacobian_nrows, const std::vector<double>& jacobian)
-{
-    const auto passed_size = static_cast<decltype(jacobian.size())>(jacobian_ncols) * jacobian_nrows;
-    if (jacobian.size() != passed_size)
-    {
-        throw std::logic_error("The actual passed jacobian size is inconsistent with the passed number of rows and columns.");
+void save_jacobian_to_file(const std::string& filepath, int jacobian_ncols,
+                           int jacobian_nrows,
+                           const std::vector<double>& jacobian) {
+    const auto passed_size =
+        static_cast<decltype(jacobian.size())>(jacobian_ncols) * jacobian_nrows;
+    if (jacobian.size() != passed_size) {
+        throw std::logic_error(
+            "The actual passed jacobian size is inconsistent with the passed "
+            "number of rows and columns.");
     }
 
     std::ofstream out(filepath);
@@ -79,13 +82,11 @@ void save_jacobian_to_file(const std::string& filepath, int jacobian_ncols, int 
     out.precision(max_digits);
     out << std::scientific;
 
-    for (auto i = 0; i < jacobian_nrows; i++)
-    {
+    for (auto i = 0; i < jacobian_nrows; i++) {
         out << jacobian[i];
 
-        for (auto j = 1; j < jacobian_ncols; j++)
-        {
-            out << "\t"  << jacobian[jacobian_nrows * j + i];
+        for (auto j = 1; j < jacobian_ncols; j++) {
+            out << "\t" << jacobian[jacobian_nrows * j + i];
         }
         out << std::endl;
     }
@@ -93,7 +94,7 @@ void save_jacobian_to_file(const std::string& filepath, int jacobian_ncols, int 
     out.close();
 }
 
-void save_sparse_j_to_file(const std::string& filepath, const BASparseMat& jacobian)
-{
+void save_sparse_j_to_file(const std::string& filepath,
+                           const BASparseMat& jacobian) {
     write_J_sparse(filepath, jacobian);
 }
--- src/cpp/runner/main.cpp
+++ src/cpp/runner/main.cpp
@@ -1,27 +1,24 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 #include "GMMBenchmark.h"
-#include "BABenchmark.h"
-#include "HandBenchmark.h"
-#include "LSTMBenchmark.h"
 
 std::string str_toupper(std::string s) {
     std::transform(s.begin(), s.end(), s.begin(),
-        [](const unsigned char c) { return std::toupper(c); }
-    );
+                   [](const unsigned char c) { return std::toupper(c); });
     return s;
 }
 
-int main(const int argc, const char* argv[])
-{
+int main(const int argc, const char* argv[]) {
     try {
         if (argc < 9) {
-            std::cerr << "usage: CPPRunner test_type module_path input_filepath output_dir minimum_measurable_time nruns_F nruns_J time_limit [-rep]\n";
+            std::cerr << "usage: CPPRunner test_type module_path "
+                         "input_filepath output_dir minimum_measurable_time "
+                         "nruns_F nruns_J time_limit [-rep]\n";
             return 1;
         }
 
@@ -29,7 +26,8 @@ int main(const int argc, const char* argv[])
         const auto module_path = argv[2];
         const string input_filepath(argv[3]);
         const string output_prefix(argv[4]);
-        const auto minimum_measurable_time = duration<double>(std::stod(argv[5]));
+        const auto minimum_measurable_time =
+            duration<double>(std::stod(argv[5]));
         const auto nruns_F = std::stoi(argv[6]);
         const auto nruns_J = std::stoi(argv[7]);
         const auto time_limit = duration<double>(std::stod(argv[8]));
@@ -38,35 +36,20 @@ int main(const int argc, const char* argv[])
         const auto replicate_point = (argc > 9 && string(argv[9]) == "-rep");
 
         if (test_type == "GMM") {
-            run_benchmark<GMMInput, GMMOutput, GMMParameters>(module_path, input_filepath, output_prefix, minimum_measurable_time, nruns_F, nruns_J, time_limit, { replicate_point });
+            run_benchmark<GMMInput, GMMOutput, GMMParameters>(
+                module_path, input_filepath, output_prefix,
+                minimum_measurable_time, nruns_F, nruns_J, time_limit,
+                {replicate_point});
+        } else {
+            throw runtime_error(
+                ("C++ runner doesn't support tests of " + test_type + " type")
+                    .c_str());
         }
-        else if (test_type == "BA") {
-            run_benchmark<BAInput, BAOutput>(module_path, input_filepath, output_prefix, minimum_measurable_time, nruns_F, nruns_J, time_limit);
-        }
-        else if (test_type == "HAND") {
-            run_benchmark<HandInput, HandOutput, HandParameters>(module_path, input_filepath, output_prefix, minimum_measurable_time, nruns_F, nruns_J, time_limit, { false });
-        }
-        else if (test_type == "HAND-COMPLICATED") {
-            run_benchmark<HandInput, HandOutput, HandParameters>(module_path, input_filepath, output_prefix, minimum_measurable_time, nruns_F, nruns_J, time_limit, { true });
-        }
-        else if (test_type == "LSTM") {
-            run_benchmark<LSTMInput, LSTMOutput>(module_path, input_filepath, output_prefix, minimum_measurable_time, nruns_F, nruns_J, time_limit);
-        }
-        else
-        {
-            throw runtime_error(("C++ runner doesn't support tests of " + test_type + " type").c_str());
-        }
-    }
-    catch (const std::exception& ex)
-    {
+    } catch (const std::exception& ex) {
         std::cerr << "An exception caught: " << ex.what() << std::endl;
-    }
-    catch (const std::string& ex)
-    {
+    } catch (const std::string& ex) {
         std::cerr << "An exception caught: " << ex << std::endl;
-    }
-    catch (...)
-    {
+    } catch (...) {
         std::cerr << "Unknown exception" << std::endl;
     }
 }
