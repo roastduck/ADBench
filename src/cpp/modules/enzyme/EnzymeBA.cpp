307621d62ca9cf2e0ffb35e62fadcdd16571ae05
trrbivial <shenji233@gmail.com>
2024-03-04 01:04:52 +0800

    fix: remove comments

--- src/cpp/modules/enzyme/EnzymeBA.cpp
+++ src/cpp/modules/enzyme/EnzymeBA.cpp
@@ -66,27 +66,15 @@ void EnzymeBA::calculate_objective(int times)
 }
 
 
-//#include<chrono>
 void EnzymeBA::calculate_jacobian(int times)
 {
 
-    //auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < times; i++)
     {
         result.J.clear();
-        //auto t1 = std::chrono::high_resolution_clock::now();
         calculate_reproj_error_jacobian_part();
-        //auto t2 = std::chrono::high_resolution_clock::now();
         calculate_weight_error_jacobian_part();
-        //auto t3 = std::chrono::high_resolution_clock::now();
-        //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-        //std::cout << "shit: " << ((float)duration.count()) / 1000000.0 << ' ';
-        //duration = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2);
-        //std::cout << ((float)duration.count()) / 1000000.0 << '\n';
     }
-    //auto end = std::chrono::high_resolution_clock::now();
-    //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    //std::cout << "times = " << times << ", calculate_jacobian = " << ((float)duration.count()) / 1000000.0 << '\n';
 }
 
 extern "C" {
@@ -103,13 +91,8 @@ extern "C" {
     );
 }
 
-//#include<iostream>
-#ifdef OMP
-//#include<omp.h>
-#endif
 void EnzymeBA::calculate_reproj_error_jacobian_part()
 {
-    //auto start = std::chrono::high_resolution_clock::now();
     //reproj_err_d容量30，reproj_err_d_row容量15
     //reproj的两个变量在后面都不会再用到了
     #ifndef OMP
@@ -124,7 +107,6 @@ void EnzymeBA::calculate_reproj_error_jacobian_part()
     double* x_gradient_part = reproj_err_d_row.data() + BA_NCAMPARAMS;
     double* weight_gradient_part = reproj_err_d_row.data() + BA_NCAMPARAMS + 3;
     #else
-    //std::cout << "fuck input p = " << input.p << '\n';
     #pragma omp parallel for
     #endif
     for (int i = 0; i < input.p; i++)
@@ -231,9 +213,6 @@ void EnzymeBA::calculate_reproj_error_jacobian_part()
         result.J.insert_reproj_err_block(i, camIdx, ptIdx, temp_save[i].data());
     }
     #endif
-    //auto end = std::chrono::high_resolution_clock::now();
-    //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    //std::cout << "calculate_reproj_error_jacobian_part = " << ((float)duration.count()) / 1000000.0 << '\n';
 }
 
 
@@ -248,26 +227,12 @@ extern "C" {
 
 void EnzymeBA::calculate_weight_error_jacobian_part()
 {
-    //auto start = std::chrono::high_resolution_clock::now();
     std::vector<double> temp_save(input.p, 0.0);
-#ifdef OMP
-    /*
-    std::cout << "omp_get_max_threads = " << omp_get_max_threads() << '\n';
-    */
-#endif
     #ifdef OMP
     #pragma omp parallel for
     #endif
     for (int j = 0; j < input.p; j++)
     {
-#ifdef OMP
-        /*
-        if (j > input.p - 10){
-            std::cout << "omp_get_num_threads = " << omp_get_num_threads() << ' ';
-            std::cout << "omp_get_thread_num = " << omp_get_thread_num() << '\n';
-        }
-        */
-#endif
         // NOTE added set of 0 here
         double wb = 0.0;         // stores calculated derivative
 
@@ -283,9 +248,6 @@ void EnzymeBA::calculate_weight_error_jacobian_part()
     for(int j = 0; j < input.p; j++){
         result.J.insert_w_err_block(j, temp_save[j]);
     }
-    //auto end = std::chrono::high_resolution_clock::now();
-    //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    //std::cout << "calculate_weight_error_jacobian_part = " << ((float)duration.count()) / 1000000.0 << '\n';
 }
 
 
